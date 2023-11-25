85e7d146b4d25cc763d22ac6c5a353dc6ba1273a
Zhoneym <Zhoneym@Outlook.com>
2023-11-26 06:53:36 +0800

    set VNDK VERSION

--- device.mk
+++ device.mk
@@ -418,7 +418,7 @@ PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_TARGET_VNDK_VERSION)
 # Update this list with what each blob is actually for
 # libstdc++: hexagon DSP blobs
 PRODUCT_PACKAGES += \
-    libstdc++.vendor
+    libstdc++_vendor
 
 # QCOM
 PRODUCT_COPY_FILES += \
